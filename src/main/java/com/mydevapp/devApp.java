e0b9d0780a7191fd54faf741ee411561faaf6bd2
SasivarniM <sasivarni00@gmail.com>
2024-07-24 17:07:43 +0530

    updated

--- src/main/java/com/mydevapp/devApp.java
+++ src/main/java/com/mydevapp/devApp.java
@@ -1,11 +1,13 @@
 package com.mydevapp;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
+@RestController
 public class devApp {
 	
 	@GetMapping("/get")
-	public String get(String name) {
+	public String get() {
 		return "Hello I am From Devops";
 	}
 
